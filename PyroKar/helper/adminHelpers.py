975ee8f5c5653899d67d4b43c67d0219730e9016
ArmanGG01 <96017229+ArmanGG01@users.noreply.github.com>
2022-12-14 01:01:25 +0700

    Update adminHelpers.py

--- PyroKar/helper/adminHelpers.py
+++ PyroKar/helper/adminHelpers.py
@@ -4,7 +4,7 @@ from time import time
 from pyrogram.types import Message
 
 from pyrogram import Client 
-from Zaid.helper.interval import IntervalHelper
+from PyroKar.helper.interval import IntervalHelper
 
 
 async def CheckAdmin(message: Message):
