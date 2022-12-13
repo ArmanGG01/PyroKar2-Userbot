3ea5ebe43e951d39218e363ef4ca4114242ee061
ArmanGG01 <96017229+ArmanGG01@users.noreply.github.com>
2022-12-14 00:04:27 +0700

    Update metrics.py

--- PyroKar/modules/basic/metrics.py
+++ PyroKar/modules/basic/metrics.py
@@ -3,7 +3,7 @@ import time
 from pyrogram import filters, Client
 from pyrogram.types import Message
 
-from Zaid.modules.help import add_command_help
+from PyroKar.modules.help import add_command_help
 
 
 class Custom(dict):
