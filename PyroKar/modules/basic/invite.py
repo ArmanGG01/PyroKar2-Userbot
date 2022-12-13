0b5ba9702793782b81d8689a97f1fc1528693c70
ArmanGG01 <96017229+ArmanGG01@users.noreply.github.com>
2022-12-14 00:00:22 +0700

    Update invite.py

--- PyroKar/modules/basic/invite.py
+++ PyroKar/modules/basic/invite.py
@@ -1,10 +1,10 @@
 from pyrogram import Client, filters
 from pyrogram.enums import ChatType, UserStatus
 from pyrogram.types import Message
-from Zaid import SUDO_USER
+from PyroKar import SUDO_USER
 from pyrogram.errors.exceptions.flood_420 import FloodWait
 
-from Zaid.modules.help import add_command_help
+from PyroKar.modules.help import add_command_help
 
 
 @Client.on_message(
