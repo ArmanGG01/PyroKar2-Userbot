eb9e0462f56cdfcfc6c0cc8ce1d11e50898b299f
ArmanGG01 <96017229+ArmanGG01@users.noreply.github.com>
2022-12-14 00:24:11 +0700

    Update bot.py

--- PyroKar/modules/bot/bot.py
+++ PyroKar/modules/bot/bot.py
@@ -3,10 +3,10 @@ import traceback
 from pyrogram import Client, filters
 from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
 
-from Zaid import CMD_HELP, app
-from Zaid.helper.data import Data
-from Zaid.helper.inline import cb_wrapper, paginate_help
-from Zaid import ids as users
+from PyroKar import CMD_HELP, app
+from PyroKar.helper.data import Data
+from PyroKar.helper.inline import cb_wrapper, paginate_help
+from PyroKar import ids as users
 
 @Client.on_callback_query()
 async def _callbacks(_, callback_query: CallbackQuery):
