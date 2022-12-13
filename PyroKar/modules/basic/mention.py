5a3d6d8f24e4ee0e02b55f42749af08705f9bfc4
ArmanGG01 <96017229+ArmanGG01@users.noreply.github.com>
2022-12-14 00:03:59 +0700

    Update mention.py

--- PyroKar/modules/basic/mention.py
+++ PyroKar/modules/basic/mention.py
@@ -5,7 +5,7 @@ from pyrogram import filters, Client
 from pyrogram.types import Message
 
 
-from Zaid.modules.help import add_command_help
+from PyroKar.modules.help import add_command_help
 
 mention = partial("<a href='tg://user?id={}'>{}</a>".format)
 
