22d2eafa9d1616f9514c023a9680c332c177dd0a
ArmanGG01 <96017229+ArmanGG01@users.noreply.github.com>
2022-12-14 00:12:35 +0700

    Update pats.py

--- PyroKar/modules/basic/pats.py
+++ PyroKar/modules/basic/pats.py
@@ -5,8 +5,8 @@ from pyrogram import filters, Client
 from pyrogram.types import Message
 
 
-from Zaid.helper.PyroHelpers import GetChatID, ReplyCheck
-from Zaid.modules.help import add_command_help
+from PyroKar.helper.PyroHelpers import GetChatID, ReplyCheck
+from PyroKar.modules.help import add_command_help
 
 
 @Client.on_message(filters.command(["pat", "pats"], ".") & filters.me)
