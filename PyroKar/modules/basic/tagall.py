86b7dde9c8a0057ce1a1b2745723dae5f276b284
ArmanGG01 <96017229+ArmanGG01@users.noreply.github.com>
2023-01-08 00:03:23 +0700

    Update tagall.py

--- PyroKar/modules/basic/tagall.py
+++ PyroKar/modules/basic/tagall.py
@@ -4,7 +4,7 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 
 
-from PyroKar.modules.help import add_command_help
+from PyroKar.modules.basic.help import add_command_help
 
 spam_chats = []
 
