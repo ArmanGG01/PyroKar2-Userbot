44dd0ce8c5dc25373e2a089e3a9fddf78f8a5b01
ArmanGG01 <96017229+ArmanGG01@users.noreply.github.com>
2023-01-08 00:05:30 +0700

    Update upload_files.py

--- PyroKar/modules/basic/upload_files.py
+++ PyroKar/modules/basic/upload_files.py
@@ -4,7 +4,7 @@ import humanize
 from pyrogram import filters, Client
 from pyrogram.types import Message
 
-from PyroKar.modules.help import add_command_help
+from PyroKar.modules.basic.help import add_command_help
 
 
 async def progress_callback(current, total, bot: Client, message: Message):
