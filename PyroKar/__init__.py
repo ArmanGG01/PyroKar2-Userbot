24b9fef7b60d104390dd05b356be2ed6bab5d38d
ArmanGG01 <96017229+ArmanGG01@users.noreply.github.com>
2023-01-08 02:07:46 +0700

    Update __init__.py

--- PyroKar/__init__.py
+++ PyroKar/__init__.py
@@ -1,27 +1,29 @@
-import logging
-import time
 import asyncio
-from pyrogram import Client
-from config import API_ID, API_HASH, SUDO_USERS, OWNER_ID, BOT_TOKEN, STRING_SESSION1, STRING_SESSION2, STRING_SESSION3, STRING_SESSION4, STRING_SESSION5, STRING_SESSION6, STRING_SESSION7, STRING_SESSION8, STRING_SESSION9, STRING_SESSION10
+import logging
+import sys
+import time
 from datetime import datetime
-
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from aiohttp import ClientSession
 from logging.handlers import RotatingFileHandler
+from typing import Any, Dict
+
+from aiohttp import ClientSession
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from gpytranslate import Translator
+from pyrogram import Client
 from pytgcalls import GroupCallFactory
 
-StartTime = time.time()
-START_TIME = datetime.now()
-CMD_HELP = {}
-SUDO_USER = SUDO_USERS
-clients = []
-ids = []
-
-SUDO_USERS.append(OWNER_ID)
-
-aiosession = ClientSession()
-
-scheduler = AsyncIOScheduler()
+from config import (
+    API_HASH,
+    API_ID,
+    BOTLOG_CHATID,
+    DB_URL,
+    STRING_SESSION1,
+    STRING_SESSION2,
+    STRING_SESSION3,
+    STRING_SESSION4,
+    STRING_SESSION5,
+    SUDO_USERS,
+)
 
 LOG_FILE_NAME = "logs.txt"
 logging.basicConfig(
@@ -46,81 +48,112 @@ LOGS = logging.getLogger(__name__)
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
 
-if API_ID:
-   API_ID = API_ID
+
+if (
+    not STRING_SESSION1
+    and not STRING_SESSION2
+    and not STRING_SESSION3
+    and not STRING_SESSION4
+    and not STRING_SESSION5
+):
+    LOGGER(__name__).error("No String Session Found! Exiting!")
+    sys.exit()
+
+if not API_ID:
+    LOGGER(__name__).error("No API_ID Found! Exiting!")
+    sys.exit()
+
+if not API_HASH:
+    LOGGER(__name__).error("No API_HASH Found! Exiting!")
+    sys.exit()
+
+if BOTLOG_CHATID:
+    BOTLOG_CHATID = BOTLOG_CHATID
 else:
-   print("WARNING: INITIALIZING APP ID ")
-   API_ID = "6435225"
+    BOTLOG_CHATID = "me"
 
-if API_HASH:
-   API_HASH = API_HASH
-else:
-   print("WARNING: INITIALIZING APP HASH ")   
-   API_HASH = "4e984ea35f854762dcde906dce426c2d"
+LOOP = asyncio.get_event_loop()
 
-if not BOT_TOKEN:
-   print("WARNING: BOT TOKEN NOT FOUND PLZ ADD ")   
+trl = Translator()
 
-app = Client(
-    name="app",
-    api_id=API_ID,
-    api_hash=API_HASH,
-    bot_token=BOT_TOKEN,
-    plugins=dict(root="PyroKar/modules/bot"),
-    in_memory=True,
+aiosession = ClientSession()
+
+CMD_HELP = {}
+
+scheduler = AsyncIOScheduler()
+
+StartTime = time.time()
+
+START_TIME = datetime.now()
+
+TEMP_SETTINGS: Dict[Any, Any] = {}
+TEMP_SETTINGS["PM_COUNT"] = {}
+TEMP_SETTINGS["PM_LAST_MSG"] = {}
+
+
+bot1 = (
+    Client(
+        name="bot1",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=STRING_SESSION1,
+        plugins=dict(root="PyroKar/modules"),
+    )
+    if STRING_SESSION1
+    else None
 )
 
-if STRING_SESSION1:
-   print("Client1: Found.. Starting..")
-   client1 = Client(name="one", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION1, plugins=dict(root="PyroKar/modules"))
-   clients.append(client1)
+bot2 = (
+    Client(
+        name="bot2",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=STRING_SESSION2,
+        plugins=dict(root="PyroKar/modules"),
+    )
+    if STRING_SESSION2
+    else None
+)
 
-if STRING_SESSION2:
-   print("Client2: Found.. Starting.. ")
-   client2 = Client(name="two", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION2, plugins=dict(root="PyroKar/modules"))
-   clients.append(client2)
+bot3 = (
+    Client(
+        name="bot3",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=STRING_SESSION3,
+        plugins=dict(root="PyroKar/modules"),
+    )
+    if STRING_SESSION3
+    else None
+)
 
-if STRING_SESSION3:
-   print("Client3: Found.. Starting.. ")
-   client3 = Client(name="three", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION3, plugins=dict(root="PyroKar/modules"))
-   clients.append(client3)
+bot4 = (
+    Client(
+        name="bot4",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=STRING_SESSION4,
+        plugins=dict(root="PyroKar/modules"),
+    )
+    if STRING_SESSION4
+    else None
+)
 
-if STRING_SESSION4:
-   print("Client4: Found.. Starting.. ")
-   client4 = Client(name="four", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION4, plugins=dict(root="PyroKar/modules"))
-   clients.append(client4)
+bot5 = (
+    Client(
+        name="bot5",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=STRING_SESSION5,
+        plugins=dict(root="PyroKar/modules"),
+    )
+    if STRING_SESSION5
+    else None
+)
 
-if STRING_SESSION5:
-   print("Client5: Found.. Starting.. ")
-   client5 = Client(name="five", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION5, plugins=dict(root="PyroKar/modules"))
-   clients.append(client5)
 
-if STRING_SESSION6:
-   print("Client6: Found.. Starting.. ")
-   client6 = Client(name="six", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION6, plugins=dict(root="PyroKar/modules"))
-   clients.append(client6)
+bots = [bot for bot in [bot1, bot2, bot3, bot4, bot5] if bot]
 
-if STRING_SESSION7:
-   print("Client7: Found.. Starting.. ")
-   client7 = Client(name="seven", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION7, plugins=dict(root="PyroKar/modules"))
-   clients.append(client7)
-
-if STRING_SESSION8:
-   print("Client8: Found.. Starting.. ")
-   client8 = Client(name="eight", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION8, plugins=dict(root="PyroKar/modules"))
-   clients.append(client8)
-
-if STRING_SESSION9:
-   print("Client9: Found.. Starting.. ")
-   client9 = Client(name="nine", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION9, plugins=dict(root="PyroKar/modules"))
-   clients.append(client9)
-
-if STRING_SESSION10:
-   print("Client10: Found.. Starting.. ")
-   client10 = Client(name="ten", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION10, plugins=dict(root="PyroKar/modules")) 
-   clients.append(client10)
-
-client = [client for client in[STRING_SESSION1, STRING_SESSION2, STRING_SESSION3, STRING_SESSION4, STRING_SESSION5, STRING_SESSION6, STRING_SESSION7, STRING_SESSION8, STRING_SESSION9, STRING_SESSION10]if client]
-for client in clients:
-    if not hasattr(client, "group_call"):
-        setattr(client, "group_call", GroupCallFactory(client).get_group_call())
+for bot in bots:
+    if not hasattr(bot, "group_call"):
+        setattr(bot, "group_call", GroupCallFactory(bot).get_group_call())
