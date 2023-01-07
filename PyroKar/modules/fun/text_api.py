63660a1f507b25f87b0dd6e7999b068420dac827
ArmanGG01 <96017229+ArmanGG01@users.noreply.github.com>
2023-01-08 00:25:23 +0700

    Update text_api.py

--- PyroKar/modules/fun/text_api.py
+++ PyroKar/modules/fun/text_api.py
@@ -4,7 +4,7 @@ from pyrogram.types import Message
 
 from PyroKar import SUDO_USER
 from PyroKar.helper.aiohttp_helper import AioHttp
-from PyroKar.modules.help import add_command_help
+from PyroKar.modules.basic.help import add_command_help
 
 text_apis_data = {
     "compliment": {
