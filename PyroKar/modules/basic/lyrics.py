cf177e8d3583d3b8d8d6a05be4162639f85da2c1
ArmanGG01 <96017229+ArmanGG01@users.noreply.github.com>
2023-01-07 23:56:33 +0700

    Update lyrics.py

--- PyroKar/modules/basic/lyrics.py
+++ PyroKar/modules/basic/lyrics.py
@@ -4,7 +4,7 @@ from pyrogram import filters, Client
 from pyrogram.types import Message
 
 from PyroKar import SUDO_USER
-from PyroKar.modules.help import add_command_help
+from PyroKar.modules.basic.help import add_command_help
 
 
 @Client.on_message(
