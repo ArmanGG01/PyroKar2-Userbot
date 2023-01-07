35f26f593c2eac1dbd42ff91dce372c686dcc7ec
ArmanGG01 <96017229+ArmanGG01@users.noreply.github.com>
2023-01-07 23:50:25 +0700

    Update profile.py

--- PyroKar/modules/basic/profile.py
+++ PyroKar/modules/basic/profile.py
@@ -9,7 +9,7 @@ from pyrogram.types import Message
 from PyroKar import SUDO_USER
 from PyroKar.helper.PyroHelpers import ReplyCheck
 
-from PyroKar.modules.help import add_command_help
+from PyroKar.modules.basic.help import add_command_help
 
 flood = {}
 profile_photo = "cache/pfp.jpg"
