de2350bf9153ef6d26aaeb891cd22634577fb8c5
ArmanGG01 <96017229+ArmanGG01@users.noreply.github.com>
2023-01-07 23:53:26 +0700

    Update define.py

--- PyroKar/modules/basic/define.py
+++ PyroKar/modules/basic/define.py
@@ -4,7 +4,7 @@ from pyrogram import filters, Client
 from pyrogram.types import Message
 
 from PyroKar.helper.aiohttp_helper import AioHttp
-from PyroKar.modules.help import add_command_help
+from PyroKar.modules.basic.help import add_command_help
 
 
 @Client.on_message(filters.command(["define", "dict"], ".") & filters.me)
