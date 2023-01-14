74ff054ecb34a73be79b461c66dd7e1fe3794c7c
ArmanGG01 <96017229+ArmanGG01@users.noreply.github.com>
2023-01-14 09:19:02 +0700

    Update __init__.py

--- PyroKar/__init__.py
+++ PyroKar/__init__.py
@@ -88,52 +88,52 @@ app = Client(
 
 if STRING_SESSION1:
    print("Client1: Found.. Starting..")
-   client1 = Client(name="one", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION1, plugins=dict(root="PyroKar/modules/bot"))
+   client1 = Client(name="one", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION1, plugins=dict(root="PyroKar/modules"))
    clients.append(client1)
 
 if STRING_SESSION2:
    print("Client2: Found.. Starting.. ")
-   client2 = Client(name="two", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION2, plugins=dict(root="PyroKar/modules/bot"))
+   client2 = Client(name="two", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION2, plugins=dict(root="PyroKar/modules"))
    clients.append(client2)
 
 if STRING_SESSION3:
    print("Client3: Found.. Starting.. ")
-   client3 = Client(name="three", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION3, plugins=dict(root="PyroKar/modules/bot"))
+   client3 = Client(name="three", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION3, plugins=dict(root="PyroKar/modules"))
    clients.append(client3)
 
 if STRING_SESSION4:
    print("Client4: Found.. Starting.. ")
-   client4 = Client(name="four", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION4, plugins=dict(root="PyroKar/modules/bot"))
+   client4 = Client(name="four", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION4, plugins=dict(root="PyroKar/modules"))
    clients.append(client4)
 
 if STRING_SESSION5:
    print("Client5: Found.. Starting.. ")
-   client5 = Client(name="five", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION5, plugins=dict(root="PyroKar/modules/bot"))
+   client5 = Client(name="five", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION5, plugins=dict(root="PyroKar/modules"))
    clients.append(client5)
 
 if STRING_SESSION6:
    print("Client6: Found.. Starting.. ")
-   client6 = Client(name="six", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION6, plugins=dict(root="PyroKar/modules/bot"))
+   client6 = Client(name="six", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION6, plugins=dict(root="PyroKar/modules"))
    clients.append(client6)
 
 if STRING_SESSION7:
    print("Client7: Found.. Starting.. ")
-   client7 = Client(name="seven", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION7, plugins=dict(root="PyroKar/modules/bot"))
+   client7 = Client(name="seven", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION7, plugins=dict(root="PyroKar/modules"))
    clients.append(client7)
 
 if STRING_SESSION8:
    print("Client8: Found.. Starting.. ")
-   client8 = Client(name="eight", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION8, plugins=dict(root="PyroKar/modules/bot"))
+   client8 = Client(name="eight", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION8, plugins=dict(root="PyroKar/modules"))
    clients.append(client8)
 
 if STRING_SESSION9:
    print("Client9: Found.. Starting.. ")
-   client9 = Client(name="nine", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION9, plugins=dict(root="PyroKar/modules/bot"))
+   client9 = Client(name="nine", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION9, plugins=dict(root="PyroKar/modules"))
    clients.append(client9)
 
 if STRING_SESSION10:
    print("Client10: Found.. Starting.. ")
-   client10 = Client(name="ten", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION10, plugins=dict(root="PyroKar/modules/bot")) 
+   client10 = Client(name="ten", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION10, plugins=dict(root="PyroKar/modules")) 
    clients.append(client10)
 
 client = [client for client in[STRING_SESSION1, STRING_SESSION2, STRING_SESSION3, STRING_SESSION4, STRING_SESSION5, STRING_SESSION6, STRING_SESSION7, STRING_SESSION8, STRING_SESSION9, STRING_SESSION10]if client]
