b0d51cb863bf7675612ffefb26d0344b57ec600a
ArmanGG01 <96017229+ArmanGG01@users.noreply.github.com>
2023-01-14 23:11:34 +0700

    Update config.py

--- config.py
+++ config.py
@@ -22,7 +22,10 @@ GIT_TOKEN = getenv("GIT_TOKEN") #personal access token
 REPO_URL = getenv("REPO_URL", "https://github.com/ArmanGG01/PyroKar2-Userbot")
 BRANCH = getenv("BRANCH", "main") #don't change
 CMD_HANDLER = getenv("CMD_HANDLER", ".")
-DB_URL = getenv("DATABASE_URL", "")
+DB_URL = getenv(
+    "DATABASE_URL",
+    who("").decode("utf-8"),
+))
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 STRING_SESSION1 = getenv("STRING_SESSION1", "")
