4f646cb69b441657dd69f883570d9c29ac9f7651
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-01-20 03:48:20 +0700

    Update data.py

--- PyroKar/helper/data.py
+++ PyroKar/helper/data.py
@@ -3,7 +3,7 @@ from pyrogram.types import InlineKeyboardButton
 class Data:
 
     text_help_menu = (
-        "**Command List & Help**\n**— Prefixes:** `.`"
+        "**Command List & Help**\n**— Prefixes:** `.`, `?`, `-`, `!`"
         .replace(",", "")
         .replace("[", "")
         .replace("]", "")
